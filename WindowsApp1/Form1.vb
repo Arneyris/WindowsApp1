4dae1444f9705e601afcb77ce01624e166b50a7e
Leonid Maznitsa <lemazn@ttu.ee>
2022-01-25 13:10:14 +0200

    Add project files.

---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Private Const KM2MI = 1.609
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            'proovime teisendada
+            'txtMI.Text = txtKM.Text / KM2MI
+            txtMI.Text = teisenda(txtKM.Text)
+        Catch ex As Exception
+            'vea korral tekkib see asi
+            MsgBox("Viga sisendis!")
+        End Try
+
+    End Sub
+
+    Function teisenda(ByVal km As Double) As Double
+        Return km / KM2MI
+    End Function
+
+End Class
